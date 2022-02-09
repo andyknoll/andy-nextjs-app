10353db9177e99464e3dffdff7646f5f1a796315
Andy Knoll <andyknoll0611@gmail.com>
2022-02-08 22:09:27 -0500

    added video and audio players

--- pages/media.js
+++ pages/media.js
@@ -1,10 +1,47 @@
 // media.js
 
+/*
+Visual Overload
+https://www.youtube.com/watch?v=90x2xX3rjuo
+
+RIT Sports
+https://youtu.be/nw_FYcd8xSw
+
+Midnight Blue
+https://youtu.be/_1ztLjGIHDM
+
+Feels Like
+https://www.youtube.com/watch?v=qEWVwynKK3M
+
+Best of Beatclub
+https://www.youtube.com/watch?v=InQ44lgY1gw
+
+HO Walkthrough
+https://www.youtube.com/watch?v=wfN8z4LCP4Q
+*/
+
+import React from 'react'
+
 import AppHeader from '../components/AppHeader'
 import AppContent from '../components/AppContent'
 import AppFooter from '../components/AppFooter'
 
+import ReactPlayer from 'react-player'
+
+const videos = [
+    {name: "Hollywood Orchestra", url: "https://www.youtube.com/watch?v=wfN8z4LCP4Q", author: "East West"},
+    {name: "Visual Overload", url: "https://www.youtube.com/watch?v=90x2xX3rjuo", author: "Andy Knoll"},
+    {name: "Best of Beatclub", url: "https://www.youtube.com/watch?v=InQ44lgY1gw", author: "Beatclub"},
+    {name: "RIT Sports Zone", url: "https://youtu.be/nw_FYcd8xSw", author: "Andy Knoll"}
+]
+
 const MediaPage = props => {
+
+    const [videoIndex, setVideoIndex] = React.useState(0);
+
+    const playVideo = (index) => {
+        setVideoIndex(index);
+    }
 
     return (
         <div className="media-page">
@@ -12,12 +49,67 @@ const MediaPage = props => {
 
             <AppContent>
                 <h3>Media Page</h3>
+
+                <div className="video-wrapper">
+                    <div className="video-player">
+                        <ReactPlayer 
+                            url={videos[videoIndex].url}
+                            controls={true}
+                        />
+                    </div>
+
+                    <div className="flex-row">
+                        <button className="button-a" onClick={() => playVideo(0)}>{videos[0].name}</button>
+                        <button className="button-a" onClick={() => playVideo(1)}>{videos[1].name}</button>
+                        <button className="button-a" onClick={() => playVideo(2)}>{videos[2].name}</button>
+                        <button className="button-a" onClick={() => playVideo(3)}>{videos[3].name}</button>
+                    </div>
+
+                    <h3>{videos[videoIndex].name}</h3>
+                    {videos[videoIndex].author}
+                </div>
             </AppContent>
 
             <AppFooter />
 
             <style jsx>
             {`
+                .video-wrapper {
+                    width: 320px;
+                    border: 1px solid red;
+                }
+                .video-player {
+                    width: 320px;
+                    height: 180px;
+                    border: 1px solid green;
+                }
+                .ReactPlayer {
+                    width: 320px !important;
+                    height: 180px !important;
+                }
+                .flex-row {
+                    display: flex;
+                    // border: 1px solid gray;
+                }
+                .button-a {
+                    margin: 5px;
+                    width: 150px;
+                    height: 40px;
+                    color: #eeeeee;
+                    background: #333333;
+                    border: none;
+                    border-radius: 5px;
+                }
+
+                @media screen and (min-width: 1200px) {
+                    .video-wrapper {
+                        width: 1000px;
+                    }    
+                    .video-player {
+                        width: 1000px;
+                    }
+                }
+
             `}
             </style>
         </div>
--- pages/music.js
+++ pages/music.js
@@ -4,6 +4,8 @@ import AppHeader from '../components/AppHeader'
 import AppContent from '../components/AppContent'
 import AppFooter from '../components/AppFooter'
 
+import ReactAudioPlayer from 'react-audio-player';
+
 const MusicPage = props => {
 
     return (
@@ -12,12 +14,43 @@ const MusicPage = props => {
 
             <AppContent>
                 <h3>Music Page</h3>
+
+                <div className="audio-player">
+                    <ReactAudioPlayer
+                        src="media/JSBach.wav"
+                        controls
+                    />
+                    <p>J.S. Bach</p>
+                    <p className="italic">"Chorus from Cantata 43"</p>
+                </div>
+
+                <div className="audio-player">
+                    <ReactAudioPlayer
+                        src="media/Purcell.wav"
+                        controls
+                    />
+                    <p>Henry Purcell</p>
+                    <p className="italic">"Sonata for Three Horns"</p>
+                </div>
+
             </AppContent>
 
             <AppFooter />
 
             <style jsx>
             {`
+                .audio-player {
+                    margin: 20px 0px 5px 0px;
+                }
+                react-audio-player {
+                    width: 500px;
+                }
+                p {
+                    margin: 5px;
+                }
+                .italic {
+                    font-style: italic;
+                }
             `}
             </style>
         </div>
